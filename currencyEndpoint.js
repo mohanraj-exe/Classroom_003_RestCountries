60a52220d31c0427846b287eeaa2e8936e995e8d
mohanraj-exe <95164416+mohanraj-exe@users.noreply.github.com>
2021-12-08 21:32:05 +0530

    Create currencyEndpoint.js

---
+++ currencyEndpoint.js
@@ -0,0 +1,18 @@
+//API-JSON parsing via jsCode
+
+//1.Creating request object
+var request = new XMLHttpRequest();
+
+//2.Open my request connection
+request.open('GET','http://api.countrylayer.com/v2/currency/EUR?access_key=d612564930cd61936df4e15c6dc1de58');
+
+//3.Send a request
+request.send();
+
+//4.Data after response
+request.onload = function(){
+    var data = JSON.parse(this.response);
+    for (let i=0; i<data.length; i++){
+        console.log(data[i]);
+    }
+}
